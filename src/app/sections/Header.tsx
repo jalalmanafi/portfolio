6ccacf7bd2e6d84f0e1d76ff1730c193af3ec485
jalalbmnf <calalmnf@gmail.com>
2025-04-16 14:20:11 +0400

    💄 Updated Header text styles

--- src/app/sections/Header.tsx
+++ src/app/sections/Header.tsx
@@ -6,8 +6,16 @@ const Header = () => {
   return (
     <div className="text-start font-medium text-base">
       <Title title="Hey 👋" />
-      <div>My name is <span className="font-bold text-xl">Manafi Jalal</span></div>
-      <div>📍 Baku, Azerbaijan</div>
+      <div>
+        My name is{" "}
+        <span className="font-bold text-xl text-nowrap">Manafi Jalal</span>
+      </div>
+      <div className="mt-1 font-medium">
+        📍
+        <span className="text-gray-800 dark:text-gray-300 opacity-60 text-xs">
+          Baku, Azerbaijan
+        </span>
+      </div>
     </div>
   );
 };
