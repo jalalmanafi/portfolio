9ab3321407048d5d458e3bc617472d9fa0da69bd
jalalbmnf <calalmnf@gmail.com>
2025-04-17 17:45:48 +0400

    🧪 Changed text

--- src/app/sections/About.tsx
+++ src/app/sections/About.tsx
@@ -10,7 +10,6 @@ const About = () => {
         I build websites that look good and work well. I love clean code and
         smart solutions. During work hours, I develop digital experiences for
         financial companies. After work, I explore new technologies and ideas.
-        This site shows what I can do. Let's make something cool together!
       </div>
     </div>
   );
